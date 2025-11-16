f3c818b6d496d313f500b3b52f72821b1640dbe2
Marcos Antonio Avila Morales <mis.redes.work@gmail.com>
2025-11-15 22:14:56 -0500

    Update nuxt.config.ts to change the app title to 'Matriuska - Aprende Ruso' and enhance meta tags for improved SEO. Added keywords, author, and various bot directives to optimize search engine indexing.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,11 +9,21 @@ export default defineNuxtConfig({
 
   app: {
     head: {
-      title: 'Practicador de Conjugaciones Rusas',
+      title: 'Matriuska - Aprende Ruso',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: 'Aprende conjugaciones de verbos rusos de forma interactiva' }
+        { name: 'description', content: 'Aprende Ruso de forma interactiva' },
+        { name: 'keywords', content: 'Ruso, Aprender Ruso, Conjugaciones, Pronunciación, Vocabulario, Gramática, Mejor apliacion para aprender Ruso, Hablar Ruso, Aprender Ruso online, Aprender Ruso gratis, Aprender Ruso facil, Aprender Ruso rapido, Aprender Ruso online gratis, Aprender Ruso online facil, Aprender Ruso online rapido' },
+        { name: 'author', content: 'Matriuska' },
+        { name: 'robots', content: 'index, follow' },
+        { name: 'googlebot', content: 'index, follow' },
+        { name: 'bingbot', content: 'index, follow' },
+        { name: 'yandexbot', content: 'index, follow' },
+        { name: 'duckduckbot', content: 'index, follow' },
+        { name: 'slurp', content: 'index, follow' },
+        { name: 'crawler', content: 'index, follow' },
+        { name: 'spider', content: 'index, follow' },
       ],
       link: [
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap' },
